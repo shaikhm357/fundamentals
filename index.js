b46cb0d2e40a6eb5bcf7ac2bf3d28700fd80a01e
Shaikh Mahboob <mahboob.shaikh@finclusionsolutions.com>
2023-11-01 08:23:54 +0530

    2. Factorial

--- index.js
+++ index.js
@@ -5,11 +5,12 @@ const n = 5
 const result = factorial(n)
 console.log(`The factorial of ${n} is ${result}`)
 
-function factorial(number) {
-    if (number === 0) {
-        return 1
-    } else {
-        // main logic
-        return (number * factorial(n - 1))
+function factorial(num) {
+
+    let result = 1
+    for (let i = 1; i <= num; i++) {
+        result = result * i
     }
+
+    return result
 }
